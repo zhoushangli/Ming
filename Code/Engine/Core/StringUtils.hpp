3315f0db0e719d4e9653afced79fcac7c8adae24
Shangli Zhou <185600557@qq.com>
2025-09-15 18:03:27 -0600

    [SD1-A02] Add error window to Bullet and Asteroid
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 425870]

---
+++ Code/Engine/Core/StringUtils.hpp
@@ -0,0 +1,12 @@
+#pragma once
+//-----------------------------------------------------------------------------------------------
+#include <string>
+
+
+//-----------------------------------------------------------------------------------------------
+const std::string Stringf( char const* format, ... );
+const std::string Stringf( int maxLength, char const* format, ... );
+
+
+
+
