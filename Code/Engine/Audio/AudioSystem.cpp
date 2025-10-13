12d866c807034ac8d1a16dfe22e9c73f3e2e722a
Shangli Zhou <185600557@qq.com>
2025-10-13 14:12:36 -0600

    [SD1-A04] Submit work
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 431713]

--- Code/Engine/Audio/AudioSystem.cpp
+++ Code/Engine/Audio/AudioSystem.cpp
@@ -11,7 +11,7 @@
 //	Downside: ALL games must now have this Code/Game/EngineBuildPreferences.hpp file.
 //
 // SD1 NOTE: THIS MEANS *EVERY* GAME MUST HAVE AN EngineBuildPreferences.hpp FILE IN ITS CODE/GAME FOLDER!!
-#include "Game/Framework/EngineBuildPreferences.hpp"
+#include "Game/EngineBuildPreferences.hpp"
 #if !defined( ENGINE_DISABLE_AUDIO )
 
 
