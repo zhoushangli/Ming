a3307b6f58f8965dd722dce2d3412a94a2300448
Shangli Zhou <185600557@qq.com>
2026-02-22 13:05:24 -0600

    [MP2-A02] Add AABB3
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 451458]

---
+++ Code/Engine/Math/AABB3.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Engine/Math/Vec2.hpp"
+
+class AABB2
+{
+public:
+	AABB2() = default;
+	AABB2(const AABB2& copy) = default;
+	~AABB2() = default;
+	explicit AABB2(const Vec2& mins, const Vec2& maxs);
+	explicit AABB2(float minX, float minY, float maxX, float maxY);
+
+	// Methods to implement
+	bool		IsPointInside(const Vec2& point) const;
+	Vec2 const	GetCenter() const;
+	Vec2 const	GetDimensions() const;
+	Vec2 const	GetNearestPoint(const Vec2& point) const;
+	Vec2 const	GetPointAtUV(const Vec2& uv) const;
+	Vec2 const	GetUVForPoint(const Vec2& point) const;
+	
+	void		Translate(const Vec2& translation);
+	void		SetCenter(const Vec2& newCenter);
+	void		SetDimensions(const Vec2& newDimensions);
+	void		StretchToIncludePoint(const Vec2& point);
+
+	bool operator==(const AABB2& other) const;
+	AABB2& operator=(const AABB2& other);
+
+public:
+	Vec2 m_mins;
+	Vec2 m_maxs;
+
+	static const AABB2 ZERO;
+	static const AABB2 UNIT;
+    static const AABB2 CENTERED_UNIT;
+};
+
