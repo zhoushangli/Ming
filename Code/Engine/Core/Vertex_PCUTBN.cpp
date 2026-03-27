7e4d5dd2f66e96b1488cfef1e8b366ff755e769b
Shangli Zhou <185600557@qq.com>
2026-03-27 06:51:21 -0600

    [SD2-A07] Add Vertext_PCUTBN
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 454989]

---
+++ Code/Engine/Core/Vertex_PCUTBN.cpp
@@ -0,0 +1,24 @@
+#include "Engine/Core/Vertex_PCU.hpp"
+
+Vertex_PCU::Vertex_PCU()
+	: m_position(0.f, 0.f, 0.f)
+	, m_color(Rgba8())
+	, m_uvTexCoords(0.f, 0.f)
+{
+}
+
+Vertex_PCU::Vertex_PCU(Vec3 const& position) 
+	: m_position(position), m_color(Rgba8()), m_uvTexCoords(0.f, 0.f)
+{
+}
+
+Vertex_PCU::Vertex_PCU(Vec3 const& position, Rgba8 const& color)
+	: m_position(position), m_color(color), m_uvTexCoords(0.f, 0.f)
+{
+}
+
+Vertex_PCU::Vertex_PCU(Vec3 const& position, Rgba8 const& color, Vec2 const& uvTexCoords) 
+	: m_position(position), m_color(color), m_uvTexCoords(uvTexCoords)
+{
+}
+
