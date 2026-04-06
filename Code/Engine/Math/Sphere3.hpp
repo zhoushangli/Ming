ccbef0a097172ce0198a72147846f3350db49bc8
Shangli Zhou <185600557@qq.com>
2026-04-05 18:36:56 -0600

    [MP2-A05] Add sphere
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 456276]

---
+++ Code/Engine/Math/Sphere3.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Engine/Math/Vec3.hpp"
+
+class AABB3
+{
+public:
+	AABB3() = default;
+	AABB3(const AABB3& copy) = default;
+	~AABB3() = default;
+
+	explicit AABB3(const Vec3& mins, const Vec3& maxs);
+	explicit AABB3(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
+
+	bool		IsPointInside(const Vec3& point) const;
+	Vec3 const	GetCenter() const;
+	Vec3 const	GetDimensions() const;
+	Vec3 const	GetNearestPoint(const Vec3& point) const;
+	Vec3 const	GetPointAtUV(const Vec3& uvw) const;
+	Vec3 const	GetUVForPoint(const Vec3& point) const;
+
+	void		Translate(const Vec3& translation);
+	void		SetCenter(const Vec3& newCenter);
+	void		SetDimensions(const Vec3& newDimensions);
+	void		StretchToIncludePoint(const Vec3& point);
+
+	bool operator==(const AABB3& other) const;
+	AABB3& operator=(const AABB3& other);
+
+public:
+	Vec3 m_mins;
+	Vec3 m_maxs;
+
+	static const AABB3 ZERO;
+	static const AABB3 UNIT;
+	static const AABB3 CENTERED_UNIT;
+};
