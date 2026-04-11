8b926373de57f2c86487d9c2d74e4fa559e9e0a4
Shangli Zhou <185600557@qq.com>
2026-04-11 15:33:44 -0600

    [SD2-A08] Rename to player controller
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 457290]

--- Code/Engine/Renderer/VertexBuffer.hpp
+++ Code/Engine/Renderer/VertexBuffer.hpp
@@ -5,25 +5,25 @@ struct ID3D11Buffer;
 
 class VertexBuffer
 {
-    friend class Renderer;
+	friend class Renderer;
 
 public:
-    VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int stride);
-    VertexBuffer(const VertexBuffer& copy) = delete;
-    virtual ~VertexBuffer();
+	VertexBuffer(ID3D11Device* device, unsigned int size, unsigned int stride);
+	VertexBuffer(const VertexBuffer& copy) = delete;
+	virtual ~VertexBuffer();
 
-    void Resize(unsigned int size);
+	void Resize(unsigned int size);
 
-    unsigned int GetSize();
-    unsigned int GetStride();
-    unsigned int GetCount();
-
-private:
-    void Create();
+	unsigned int GetSize();
+	unsigned int GetStride();
+	unsigned int GetCount();
 
 private:
-    ID3D11Device* m_device = nullptr;
-    ID3D11Buffer* m_buffer = nullptr;
-    unsigned int m_size = 0;
-    unsigned int m_stride = 0;
+	void Create();
+
+private:
+	ID3D11Device* m_device = nullptr;
+	ID3D11Buffer* m_buffer = nullptr;
+	unsigned int  m_size   = 0;
+	unsigned int  m_stride = 0;
 };
