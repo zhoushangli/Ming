409d9c18258ad9d028814bd0727d41545c939c0c
Shangli Zhou <185600557@qq.com>
2026-04-28 09:08:02 -0600

    [SD2-A10] Try to add a A* navigator
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 459904]

---
+++ Code/Engine/Math/Capsule3.cpp
@@ -0,0 +1,3 @@
+#include "Engine/Math/Capsule3.hpp"
+
+Capsule3::Capsule3(Vec3 const& start, Vec3 const& end, float radius) : m_start(start), m_end(end), m_radius(radius) {}
---
+++ Code/Engine/Math/Capsule3.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Engine/Math/Vec3.hpp"
+
+class Capsule3
+{
+public:
+    Capsule3() = default;
+    Capsule3(Vec3 const& start, Vec3 const& end, float radius);
+    ~Capsule3() = default;
+
+public:
+    Vec3  m_start  = Vec3::ZERO;
+    Vec3  m_end    = Vec3::ZERO;
+    float m_radius = 0.f;
+};
