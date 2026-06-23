bacbba5957036e3ee1c24eda5ff4c6fa30e7675c
Shangli Zhou <185600557@qq.com>
2026-06-22 21:05:02 -0600

    [DFS1] Add resource
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 463941]

---
+++ Code/MingEngine/Core/Object/Ref.cpp
@@ -0,0 +1,45 @@
+#include "MingEngine/Core/Object/Ref.hpp"
+
+bool RefCounted::InitRef()
+{
+	if (m_refCount != 0)
+	{
+		return false;
+	}
+
+	m_refCount = 1;
+	return true;
+}
+
+bool RefCounted::Ref()
+{
+	if (m_refCount <= 0)
+	{
+		return false;
+	}
+
+	m_refCount++;
+	return true;
+}
+
+bool RefCounted::Unref()
+{
+	if (m_refCount <= 0)
+	{
+		return false;
+	}
+
+	m_refCount--;
+	if (m_refCount == 0)
+	{
+		delete this;
+		return true;
+	}
+
+	return false;
+}
+
+int RefCounted::GetRefCount() const
+{
+	return m_refCount;
+}
--- Code/MingEngine/Core/Object/Ref.hpp
+++ Code/MingEngine/Core/Object/Ref.hpp
@@ -1,1 +1,104 @@
 #pragma once
+
+#include "MingEngine/Core/Object/Object.hpp"
+
+class RefCounted : public Object
+{
+public:
+	RefCounted()          = default;
+	virtual ~RefCounted() = default;
+
+	bool InitRef();
+	bool Ref();
+	bool Unref();
+
+	int GetRefCount() const;
+
+private:
+	int m_refCount = 0;
+};
+
+template <typename T>
+class Ref
+{
+	static_assert(std::is_base_of_v<RefCounted, T>, "Ref<T> can only be used with classes derived from RefCounted.");
+
+public:
+	Ref() = default;
+	explicit Ref(T* ptr) { RefPointer(ptr); }
+	Ref(Ref<T> const& other) { RefPointer(other.reference); }
+	Ref(Ref<T>&& other) noexcept
+	{
+		RefPointer(other.reference);
+		other.RefPointer(nullptr);
+	}
+	~Ref() { RefPointer(nullptr); }
+
+	Ref& operator=(Ref<T> const& other)
+	{
+		if (this != &other)
+		{
+			RefPointer(other.reference);
+		}
+		return *this;
+	}
+
+	Ref& operator=(Ref<T>&& other) noexcept
+	{
+		if (this != &other)
+		{
+			RefPointer(other.reference);
+			other.RefPointer(nullptr);
+		}
+		return *this;
+	}
+
+	T* Get() const { return reference; }
+	T* operator->() { return reference; }
+	T& operator*() { return *reference; }
+
+	bool IsValid() const { return reference != nullptr; }
+	bool IsNull() const { return reference == nullptr; }
+
+	bool operator==(const Ref<T>& other) const { return reference == other.reference; }
+	bool operator!=(const Ref<T>& other) const { return reference != other.reference; }
+	bool operator==(const T* ptr) const { return reference == ptr; }
+	bool operator!=(const T* ptr) const { return reference != ptr; }
+
+private:
+	void RefPointer(T* ptr)
+	{
+		if (ptr == reference)
+		{
+			return;
+		}
+
+		// 1) Unref the current reference if it exists
+		if (reference != nullptr)
+		{
+			if (reference->Unref())
+			{
+				delete reference;
+				reference = nullptr;
+			}
+		}
+
+		reference = ptr;
+
+		// 2) Ref the new reference if it exists
+		if (reference)
+		{
+			if (reference->GetRefCount() == 0)
+			{
+				reference->InitRef();
+			}
+			else
+			{
+				reference->Ref();
+			}
+		}
+	}
+
+private:
+	T* reference = nullptr;
+};
