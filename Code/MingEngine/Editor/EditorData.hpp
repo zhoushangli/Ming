e8d4f06f3ef92b09f2e48ac7f0a2750ab0f38cc7
Shangli Zhou <185600557@qq.com>
2026-07-12 06:55:53 -0600

    [DFS1] Add Editor Data
    [git-p4: depot-paths = "//depot/C35/Students/shangliz/SD/Engine/": change = 465299]

---
+++ Code/MingEngine/Editor/EditorData.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "MingEngine/Scene/Core/Node.hpp"
+#include "MingEngine/Scene/Core/NodeHandle.hpp"
+#include "MingEngine/Core/Math/Vec2.hpp"
+#include "MingEngine/Editor/UI/EditorUIContext.hpp"
+
+class Camera3D;
+class EditorCamera;
+class EditorUI;
+class EditorGizmos;
+
+class EditorSelection
+{
+public:
+	NodeHandle GetSelected() const;
+	void       SetSelected(NodeHandle handle);
+	void       Clear();
+
+private:
+	NodeHandle m_selectedNodeHandle = NodeHandle::Invalid;
+};
+
+class EditorNode : public Node
+{
+	MCLASS(EditorNode, Node);
+
+public:
+	EditorNode();
+	~EditorNode() override;
+
+	static EditorNode* Get();
+
+	EditorSelection&       GetSelection();
+	EditorSelection const& GetSelection() const;
+
+	EditorCamera* GetEditorCamera() const { return m_editorCamera; }
+
+	// --- Mouse event routing ---
+	void OnMouseMove(Vec2 screenPos, Vec2 delta);
+	void OnMouseDown(int keyCode, Vec2 screenPos);
+	void OnMouseUp(int keyCode, Vec2 screenPos);
+
+private:
+	void OnReady() override;
+	void OnProcess(float deltaSeconds) override;
+
+	void SaveSceneToFile(Node const* sceneRoot, std::string const& virtualPath);
+
+public:
+	EditorSelection m_selection;
+	EditorGizmos*   m_editorGizmos = nullptr;
+	EditorCamera*   m_editorCamera = nullptr;
+	EditorUI*       m_editorUI     = nullptr;
+
+private:
+	static EditorNode* s_instance;
+
+	EditorData m_editorData;
+	EditorUIContext m_uiContext;
+};
